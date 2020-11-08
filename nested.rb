1cc3f4269d9a086ad6914d1d3f725b42af89e93e
John Fuquene <jfuquene@email.wm.edu>
2020-11-08 22:46:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry' 
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+ programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,16 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:additonal_characters] = [
+   binding.pry
+    {
+    :yukihiro_matsumoto => {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+    }
+    }
+  ]
+  programmer_hash
 end
 
 def changing_alan
